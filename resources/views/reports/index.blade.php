8568f27a048847409548770625badabbbec1dc9b
rachmanj <rachmanj@gmail.com>
2021-09-20 09:55:37 +0800

    minor update

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -20,7 +20,9 @@
               <li><a href="{{ route('reports.report1') }}">List Additional Documents (<b>tabel doktam</b>) yg belum ada invoicenya / belum terhubung dgn invoice (<b>tabel irr5_invoice)</b></a>.</li>
               <li><a href="{{ route('reports.report2') }}">List Invoice vs Additional Document dgn PO yg sama dan belum terhubung.</a></li>
               <li><a href="{{ route('reports.report3') }}">Cek Additional Docs di table <b>irr5_rec_addoc</b>, dan jika ada dicopy ke table <b>doktams</b>.</a></li>
+              @if (Auth()->user()->role == 'SUPERDMIN' || Auth()->user()->role == 'ADMINACC')
               <li><a href="{{ route('reports.report4') }}">Invoice dgn Additional Documents lengkap, namun belum di-SPI-kan.</a></li>
+              @endif
             </ol>
           </div>
         </div>
