0ccf4d89f291585bc45c6ef1124dce69f535053f
rachmanj <rachmanj@gmail.com>
2021-09-20 09:25:45 +0800

    minor report

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -140,7 +140,7 @@ class ReportsController extends Controller
                         ->whereNull('mailroom_bpn_date')
                         ->whereNull('sent_status')
                         ->where('receive_place', 'BPN')
-                        ->orderby('receive_date', 'desc')
+                        ->orderby('inv_date', 'asc')
                         ->get();
 
         return datatables()->of($invoices)
