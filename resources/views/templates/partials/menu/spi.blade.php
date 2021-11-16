501bb99454839156a32260fb40e4c2ceb0869095
rachmanj <rachmanj@gmail.com>
2021-11-16 09:16:33 +0800

    class menu open for menu spis

--- resources/views/templates/partials/menu/spi.blade.php
+++ resources/views/templates/partials/menu/spi.blade.php
@@ -1,5 +1,5 @@
-<li class="nav-item">
-  <a href="#" class="nav-link">
+<li class="nav-item {{ request()->is('spis') || request()->is('spis/*') ? 'menu-open' : '' }}">
+  <a href="#" class="nav-link {{ request()->is('spis') || request()->is('spis/*') ? 'active' : '' }}">
     <i class="nav-icon fas fa-folder"></i>
     <p>
       SPI
@@ -8,7 +8,7 @@
   </a>
   <ul class="nav nav-treeview">
     <li class="nav-item">
-      <a href="{{ route('spis.create.index') }}" class="nav-link">
+      <a href="{{ route('spis.create.index') }}" class="nav-link {{ request()->is('spis') || request()->is('spis/*') ? 'active' : '' }}">
         <i class="far fa-circle nav-icon"></i>
         <p>Receive SPI / LPD</p>
       </a>
