1dda85a2df954c9d978232f8a8c6d134315b9de0
rachmanj <rachmanj@gmail.com>
2022-04-11 16:43:47 +0800

    disable softdeletes pada model Doktam

--- app/Models/Doktam.php
+++ app/Models/Doktam.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Doktam extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
     protected $guarded = [];
     protected $with = ['doctype', 'invoice'];
 
